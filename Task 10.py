8f70ce662471af33c2923640370ac756b3c8077b
DaKashtan <93709254+DaKashtan@users.noreply.github.com>
2021-12-22 23:28:31 +0300

    Update Task 10.py

--- Task 10.py
+++ Task 10.py
@@ -1,6 +1,7 @@
 def alph():
   A = input('Задайте алфавит - ').split()
   seq = input('Введите последовательность - ')
-  for i in range(len(seq)):
+  def statistic():
+    for i in range(len(seq)):
       G = seq.count(A[i])
       print(A[i], G)
